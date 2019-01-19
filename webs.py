14b35732cfb778df4efb0e3fc4e22da717fba8b6
rajeshm9 <rajeshmahajan09@gmail.com>
2019-01-19 21:26:23 +0530

    Added Database connection

--- webs.py
+++ webs.py
@@ -14,18 +14,24 @@ import time
 from bs4 import BeautifulSoup
 
 
-url="https://www.naukri.com/devops-jobs"
 url="https://www.usatoday.com/"
 url="https://github.com/rajeshm9?tab=repositories"
+url="https://www.naukri.com/devops-jobs"
+
+
 '''
 VERSION BUILD=1005 RECORDER=CR
 URL GOTO=https://www.naukri.com/devops-jobs
 TAB OPEN
 TAG POS=1 TYPE=A ATTR=ID:jdUrl
 TAG POS=1 TYPE=BUTTON ATTR=TXT:Next
-
-
 '''
+
+conn = sqlalchemy.create_engine('mysql+pymysql://root:raj#123@localhost/webscrap')
+config = pd.read_sql_query("select * from config", conn) 
+
+print(config)
+
 options = Options()
 #options.set_headless(headless=True)
 #options.add_argument("--headless")
@@ -38,22 +44,42 @@ def getPageSrc (url):
     driver.close()
     return src
 
+def readFromFile(fileName):
+    with open(fileName, 'r') as content_file:
+        return content_file.read()
+
 
 
 def findallLinks(soup, link, next):
     
-    for a in soup.find_all(link['type'] , {link['attr']:}):
+    d = config['mainlinkConfig'].str.split("|")[0]
+    for a in soup.find_all(d[0], {d[1]: d[2]}):
         print(a.get('href'))
         
+        
+    d = config['mainlinkNextConfig'].str.split("|")[0]
+    try:
+        pageNext = soup.find(d[0], {d[1]: d[2]}).find("a").get('href')
+        print(pageNext)
+    except:
+         pageNext = None
     
+'''    
 html_doc = getPageSrc(url)
+file = open("tt.html", "w")
+file.write(html_doc)
+file.close()
+'''
+
+html_doc = readFromFile('tt.html')
+
 soup = BeautifulSoup(html_doc, 'html.parser')
 
-link['type'] = 'a'
-link['attr'] = 'id'
-link['val'] ='jdUrl'
+
+next = dict()
+
 #findallLinks (soup, "a", "id", "jdUrl", "0")
-findallLinks (soup, "a", "id", "jdUrl", "0")
+findallLinks (soup, link, "0")
 #findallLinks(soup, "a", "class", "js-asset-link", 0)
 #findallLinks(soup, "a", "itemprop", "name codeRepository", 0)
 
